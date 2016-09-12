dc5951117b0e1a0b5653e40c77a2e6d8ccda7963
Tri Nguyen <minhtri.mtn@hotmail.com>
2016-09-12 23:59:29 +0700

    add contacts view

--- Ftr.Services/Controllers/ContactsController.fs
+++ Ftr.Services/Controllers/ContactsController.fs
@@ -1,16 +1,29 @@
 ﻿namespace Ftr.Services.Controllers
 
 open System.Web.Http
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
 open Ftr.Services.Models
 open Ftr.Services.Repositories
 
 type ContactsController(repository : ContactsRepository) =
-    inherit ApiController()
-
+    //inherit ApiController()
+    inherit Controller()
+    
     new() = new ContactsController(new ContactsRepository())
 
-    member x.Get() = 
-        repository.GetAll()
+    member this.Index () = 
+        let contacts = repository.GetAll()
+        this.View()
+
+    
+
+//    member x.Get() = 
+//        repository.GetAll()
 
     member x.Post ([<FromBody>] contact) =    
         repository.AddNew(contact)
