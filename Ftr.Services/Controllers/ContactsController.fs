1bf5c82c4a281ef6b32b464aa35ec4f9fa50e481
Tri Nguyen <minhtri.mtn@hotmail.com>
2016-09-12 23:03:54 +0700

    added EF Models, Respositories

---
+++ Ftr.Services/Controllers/ContactsController.fs
@@ -0,0 +1,16 @@
+﻿namespace Ftr.Services.Controllers
+
+open System.Web.Http
+open Ftr.Services.Models
+open Ftr.Services.Repositories
+
+type ContactsController(repository : ContactsRepository) =
+    inherit ApiController()
+
+    new() = new ContactsController(new ContactsRepository())
+
+    member x.Get() = 
+        repository.GetAll()
+
+    member x.Post ([<FromBody>] contact) =    
+        repository.AddNew(contact)
